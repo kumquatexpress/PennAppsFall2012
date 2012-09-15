74cc6514b47c0a55561e90d2b64e38bdbb80930a
phulce <phulce@MeinAutomobil>
2012-09-15 03:18:02 -0400

    Updated UI.

--- webapp/js/ui.js
+++ webapp/js/ui.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
     $('#search_form').submit(function() {
-              event.preventDefault();
-              $('.overlay').each(function() {
-                            $(this).fadeOut();
-                    });
-              $('#map_canvas').animate({width: "75%"},2000,function(){
-                                                                                                                            $('#sidebar').css('z-index','1');
-                                                                                                                            });
+          event.preventDefault();
+          $('.overlay').each(function() {
+                        $(this).fadeOut();
+                });
+          $('#map_canvas').animate({width: "75%"},2000,function(){
+                $('#sidebar').css('z-index','1');
+          });
       });
-    $('#')
+    $('#search_input').focus();
+    $('.sublet').hover(function() {
+        $(this).toggleClass('hovered');
+        $(this).animate({height: "100px"},300);
+    }, function(){
+        $(this).animate({height: "60px"},300,function() {
+            $(this).toggleClass('hovered');
+        });
+    });
 });
