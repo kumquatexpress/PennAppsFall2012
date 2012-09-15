92128b5b5f32069d6af3e51af45bb54d8a7eec29
phulce <phulce@MeinAutomobil>
2012-09-15 02:05:18 -0400

    Updated mapping to import JSON from crime_points.

--- webapp/index.php
+++ webapp/index.php
@@ -10,11 +10,38 @@
     <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script type="text/javascript" src="js/jquery-1.8.1.js"></script>
     <script type="text/javascript" src="js/maps.js"></script>
+
   </head>
     <link rel=stylesheet href="css/style.css" type="text/css" media=screen>
-  <body>
-
+<body>
+  <div id="search_overlay" class="overlay"></div>
+    <form id="search_form">
+    <input class="overlay" placeholder="search for a place..." id="search_input" />
+    </form>
     <div id="map_canvas"></div>
-
+<div id="sidebar">
+	<div class='content'>
+        <div class="controls">
+            <a class='search' href='#'>
+            	<img src='img/back_64.png'/><span>search</span>
+            </a>
+            <a class='back' href='#'>
+            	<img src='img/back_64.png'/><span>back</span>
+            </a>
+        </div>
+        <div style="clear:both"></div>
+        <div class="results">
+            <div class="sublet">
+            	<div class="glance">
+                    <div class="rating"><img src="img/32/80.png" /></div>
+                    <div class="price">$900</div>
+                </div>
+                <div class='info'>
+                    <div class="title"><span class="type">2BR 3BATH</span></div>
+                    <div class="description">This is a fine apartment over lookin the river on a nice lake in front of a city dump. Call for details!</div>
+            </div>
+        </div>
+    </div>
+</div>
   </body>
 </html>
---
+++ webapp/js/ui.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+						   $('#search_form').submit(function() {
+		  event.preventDefault();
+		  $('.overlay').each(function() {
+				$(this).fadeOut();
+			});
+		  $('#map_canvas').animate({width: "75%"},2000,function(){
+																$('#sidebar').css('z-index','1');
+																});
+	  });
+						   });
